b960ca211e74ad87fb6afa1a9396b1e179c8f206
saldanaj27 <jes921@nyu.edu>
2025-08-25 19:38:21 -0700

    route API endpoints to respective apps

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+
+router.register(r'teams', views.TeamViewSet, basename='team')
+router.register(r'players', views.PlayerViewSet, basename='player')
+router.register(r'games', views.GameViewSet, basename='game')
+router.register(r'stats', views.PlayerStatViewSet, basename='stat')
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
