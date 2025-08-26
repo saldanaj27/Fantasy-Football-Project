e89104500d183a887ed6c66211b1146909bc45df
saldanaj27 <jes921@nyu.edu>
2025-08-25 19:37:51 -0700

    create viewsets for models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from teams.models import Team
+from players.models import Player
+from games.models import Game
+from stats.models import FootballPlayerGameStat
 
-# Create your views here.
+from .serializers import (
+    TeamSerializer,
+    PlayerSerializer,
+    GameSerializer,
+    FootballPlayerGameStatSerializer,
+)
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+class PlayerStatViewSet(viewsets.ModelViewSet):
+    queryset = FootballPlayerGameStat.objects.all()
+    serializer_class = FootballPlayerGameStatSerializer
