d38166d9a4b317b1da3eb858f0c93f8387f49fb3
saldanaj27 <jes921@nyu.edu>
2025-08-30 19:53:46 -0700

    add auth views

--- api/views.py
+++ api/views.py
@@ -1,17 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 from teams.models import Team
 from players.models import Player
 from games.models import Game
 from users.models import User
 from stats.models import FootballPlayerGameStat
-
 from .serializers import (
     TeamSerializer,
     PlayerSerializer,
     GameSerializer,
     FootballPlayerGameStatSerializer,
-    UserSerializer
+    UserSerializer,
+    RegisterSerializer,
+    LoginSerializer,
 )
+
+User = get_user_model()
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -32,3 +39,23 @@ class GameViewSet(viewsets.ModelViewSet):
 class PlayerStatViewSet(viewsets.ModelViewSet):
     queryset = FootballPlayerGameStat.objects.all()
     serializer_class = FootballPlayerGameStatSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user_id": user.id, "username": user.username})
+
+class LogoutView(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({"message": "Logged out successfully"})
