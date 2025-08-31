619659a70242d89c5c663966f9c91e8389b07f09
saldanaj27 <jes921@nyu.edu>
2025-08-30 19:53:18 -0700

    add auth user serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 from teams.models import Team
 from players.models import Player
 from games.models import Game
 from stats.models import FootballPlayerGameStat
 from users.models import User
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,3 +34,29 @@ class FootballPlayerGameStatSerializer(serializers.ModelSerializer):
     class Meta:
         model = FootballPlayerGameStat
         fields = "__all__"
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password")
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"],
+        )
+        Token.objects.create(user=user)  # auto-create token
+        return user
