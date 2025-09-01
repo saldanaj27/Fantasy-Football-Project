84ffee908ba08bbd8a5d56a8fb67ad07ddad7b13
saldanaj27 <jes921@nyu.edu>
2025-09-01 16:21:37 -0700

    remove loginview and logoutview

--- api/views.py
+++ api/views.py
@@ -1,7 +1,4 @@
 from rest_framework import viewsets, generics, permissions
-from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from teams.models import Team
 from players.models import Player
@@ -15,7 +12,6 @@ from .serializers import (
     FootballPlayerGameStatSerializer,
     UserSerializer,
     RegisterSerializer,
-    LoginSerializer,
 )
 
 User = get_user_model()
@@ -44,18 +40,3 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
-
-class LoginView(APIView):
-    permission_classes = [permissions.AllowAny]
-
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key, "user_id": user.id, "username": user.username})
-
-class LogoutView(APIView):
-    def post(self, request):
-        request.user.auth_token.delete()
-        return Response({"message": "Logged out successfully"})
