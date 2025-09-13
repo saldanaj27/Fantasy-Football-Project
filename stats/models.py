5ba38114c3a186d0f0cc8950cf7dc6c890d52372
saldanaj27 <jes921@nyu.edu>
2025-09-12 20:26:01 -0700

    add more stat fields

--- stats/models.py
+++ stats/models.py
@@ -8,23 +8,25 @@ class FootballPlayerGameStat(models.Model):
 
     rush_attempts = models.PositiveIntegerField(default=0)
     rush_yards = models.IntegerField(default=0)
+    rushing_touchdowns = models.PositiveIntegerField(default=0)
     fumbles = models.PositiveIntegerField(default=0)
 
     pass_yards = models.IntegerField(default=0)
     pass_completions = models.PositiveIntegerField(default=0)
     pass_attempts = models.PositiveIntegerField(default=0)
-
-    touchdowns = models.PositiveIntegerField(default=0)
+    completion_pct = models.DecimalField(max_digits=5, decimal_places=2)
+    sacks = models.PositiveIntegerField(default=0)
+    passing_touchdowns = models.PositiveIntegerField(default=0)
     interceptions = models.PositiveIntegerField(default=0)
 
     targets = models.PositiveIntegerField(default=0)
     receptions = models.PositiveIntegerField(default=0)
     receiving_yards = models.IntegerField(default=0)
+    receiving_touchdowns = models.IntegerField(default=0)
+
+    two_pt = models.PositiveIntegerField(default=0)
 
     fantasy_points = models.IntegerField(default=0)
-
-    class Meta:
-        unique_together = ("player", "game")
 
     def __str__(self):
         return f"{self.player} - {self.game}"
