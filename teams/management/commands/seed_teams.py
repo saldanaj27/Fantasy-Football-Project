b6073ab842486a7d3f2762c597835940a4748ec0
saldanaj27 <jes921@nyu.edu>
2025-09-29 22:41:37 -0700

    update seed_teams with nflreadpy library

--- teams/management/commands/seed_teams.py
+++ teams/management/commands/seed_teams.py
@@ -1,36 +1,28 @@
-import requests
 from django.core.management.base import BaseCommand
 from teams.models import Team
 from django.conf import settings
+import nflreadpy as nfl
 
 class Command(BaseCommand):
-    help = "Seed Teams data from Sports API from specific season"
+    help = "Seed Teams table with NFl team data from nflreadpy (using 2025 season)"
+    
     def handle(self, *args, **kwargs):
-        season = "2023"
-        api_key = settings.SPORTS_API_KEY
-        url = "https://v1.american-football.api-sports.io/teams?league=1&season=" + season
+        curr_season = 2025
+        teams_df = nfl.load_teams()
 
-        headers = {
-            "x-apisports-key": api_key,
-            "x-rapidapi-host": "v1.american-football.api-sports.io"
-        }
-        response = requests.get(url, headers=headers)
+        for row in teams_df.filter(teams_df["season"] == curr_season).iter_rows(named=True):
+            team_id = row['nfl_team_id']
+            team_name = row['full']
+            team_abb = row['team']
+            team_city = row['location']
 
-        if response.status_code != 200:
-            self.stderr.write(self.style.ERROR("Failed to fetch teams from API"))
-            return
-        
-        data = response.json().get("response", [])
+            Team.objects.update_or_create(
+                id = team_id,
+                defaults={
+                    "name": team_name, 
+                    "abbreviation": team_abb, 
+                    "city": team_city
+                }
+            )
 
-        for team in data:
-            if team["name"] != 'NFC' or team["name"] != 'AFC':
-                team_id = team["id"]
-                name = team["name"]
-                city = team.get("city", "") # default is empty string
-                abbreviation = team.get("code", "")
-                logo_url = team.get("logo", "")
-                
-                Team.objects.update_or_create(
-                    id=team_id,
-                    defaults={"name": name, "city": city, "abbreviation": abbreviation, "logo_url": logo_url},
-                )
+
