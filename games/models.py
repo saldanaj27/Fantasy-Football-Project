260ae5722d61f8db19a9b25807e0a4f420a82ce7
saldanaj27 <jes921@nyu.edu>
2025-11-25 16:05:39 -0800

    update game model for nflreadpy

--- games/models.py
+++ games/models.py
@@ -1,55 +1,52 @@
 from django.db import models
 from teams.models import Team
-from django.conf import settings
+from .constants import STAGE_CHOICES
 
-class Score(models.Model):
-    quarter_1 = models.IntegerField(default=0)
-    quarter_2 = models.IntegerField(default=0)
-    quarter_3 = models.IntegerField(default=0)
-    quarter_4 = models.IntegerField(default=0)
-    overtime = models.IntegerField(default=0)
+# class Score(models.Model):
+#     quarter_1 = models.IntegerField(default=0)
+#     quarter_2 = models.IntegerField(default=0)
+#     quarter_3 = models.IntegerField(default=0)
+#     quarter_4 = models.IntegerField(default=0)
+#     overtime = models.IntegerField(default=0)
     
-    @property
-    def total(self):
-        return (
-            self.quarter_1
-            + self.quarter_2
-            + self.quarter_3
-            + self.quarter_4
-            + (self.overtime or 0)
-        )
+#     @property
+#     def total(self):
+#         return (
+#             self.quarter_1
+#             + self.quarter_2
+#             + self.quarter_3
+#             + self.quarter_4
+#             + (self.overtime or 0)
+#         )
 
-    def __str__(self):
-        return f"Total: {self.total}"
+#     def __str__(self):
+#         return f"Total: {self.total}"
 
 class Game(models.Model): 
-    STATUS_CHOICES = [
-        ('NS', 'Not Started'),
-        ('Q1', 'First Quarter'),
-        ('Q2', 'Second Quarter'),
-        ('Q3', 'Third Quarter'),
-        ('OT', 'Overtime'),
-        ('HT', 'Halftime'),
-        ('FT', 'Finished'),
-        ('CANC', 'Canceled'),
-        ('PST', 'Postponed'),
-    ]
-
-    STAGE_CHOICES = [
-        ('PRE', 'Pre Season'),
-        ('REG', 'Regular Season'),
-        ('POST', 'Post Season}'),
-    ]
-
-    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_games')
-    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_games')
-    date = models.DateTimeField()
-    week = models.IntegerField(default=0)
+    id = models.CharField(max_length=20, primary_key=True)
     season = models.IntegerField(default=0)
+    week = models.IntegerField(default=0)
+    date = models.DateTimeField()
+    time = models.CharField(max_length=10, default='00:00')
+
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team')
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_team')
+    
     stage = models.CharField(max_length=20, choices=STAGE_CHOICES, default='REG')
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='NS')
-    home_score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='home_score')
-    away_score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='away_score')
+
+    # replace if able to retrieve quarter scores
+    # home_score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='home_score')
+    # away_score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='away_score')
+
+    home_score = models.IntegerField(default=0, null=True)
+    away_score = models.IntegerField(default=0, null=True)
+    total_score = models.IntegerField(default=0, null=True)
+    overtime = models.BooleanField(default=False, null=True)
+
+    location = models.CharField(max_length=15, default='Home')
+    roof = models.CharField(max_length=15, default='outdoors')
+    temp = models.IntegerField(default=0, null=True)
+    wind = models.IntegerField(default=0, null=True)
 
     def __str__(self):
         return f"{self.away_team} @ {self.home_team} - Week {self.week}"
