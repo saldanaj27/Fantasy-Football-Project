a851ac64f8d694d0438e7ae584f2ec4d73d1bc27
saldanaj27 <jes921@nyu.edu>
2025-11-25 16:02:10 -0800

    update player model to add values + primary key

--- players/models.py
+++ players/models.py
@@ -2,14 +2,19 @@ from django.db import models
 from teams.models import Team
 from .constants import POSITION_CHOICES, DEPTH_CHART_POSITION_CHOICES, STATUS
 
-class Player(models.Model):    
+class Player(models.Model):
+    id = models.CharField(max_length=25, primary_key=True)
+    
     name = models.CharField(max_length=100)
-    depth_chart_position = models.CharField(max_length=25, choices=DEPTH_CHART_POSITION_CHOICES)
     height = models.IntegerField(default=0, null=True)
     weight = models.IntegerField(default=0, null=True)
+    image_url = models.URLField(blank=True, null=True)
+
+    depth_chart_position = models.CharField(max_length=25, choices=DEPTH_CHART_POSITION_CHOICES)
     position = models.CharField(max_length=25, choices=POSITION_CHOICES)
     status = models.CharField(max_length=30, choices=STATUS)
-    image_url = models.URLField(blank=True, null=True)
+    
+    season = models.IntegerField(default=0000, null=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
