59eace4ab31f3b91a6d72be355965bb4a25e573a
saldanaj27 <jes921@nyu.edu>
2025-12-08 22:25:28 -0500

    add APIs to GameViewSet

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,7 @@
+from django.utils import timezone
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from teams.models import Team
 from players.models import Player
 from games.models import Game
@@ -21,6 +24,40 @@ class PlayerViewSet(viewsets.ModelViewSet):
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
+    # add filtering logic for 'week' and 'season' to query
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        week = self.request.query_params.get('week')
+        season = self.request.query_params.get('season')
+
+        if week:
+            qs = qs.filter(week=week)
+
+        if season:
+            qs = qs.filter(season=season)
+        
+        return qs
+    
+    # path to get current week (/current_week)
+    @action(detail=False, methods=['get'])
+    def current_week(self, request):
+        today = timezone.now().date()
+
+        # find nearest game and use its week
+        upcoming = Game.objects.filter(date__gte=today).order_by('date').first()
+        if not upcoming:
+            return Response([])
+        
+        week = upcoming.week
+        season = upcoming.season
+
+        games = Game.objects.filter(week=week, season=season)
+        serializer = self.get_serializer(games, many=True)
+        
+        return Response(serializer.data)
+
 
 class PlayerStatViewSet(viewsets.ModelViewSet):
     queryset = FootballPlayerGameStat.objects.all()
