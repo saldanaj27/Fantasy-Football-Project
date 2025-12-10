deba87c9e0ffd71a8c02c30c2570b97ecf9aae23
saldanaj27 <jes921@nyu.edu>
2025-12-10 00:05:20 -0500

    add current week api

--- frontend/src/api/games.js
+++ frontend/src/api/games.js
@@ -1,6 +1,11 @@
 import api from "./client";
 
 export async function getGames(season, week) {
-  const response = await api.get(`/games/?season=${season}&week=${week}`);
-  return response.data;
+  const response = await api.get(`/games/?season=${season}&week=${week}`)
+  return response.data
+}
+
+export async function getCurrentWeekGames() {
+  const response = await api.get(`/games/currentWeek/`)
+  return response.data
 }
