ab5c9f78326841e436f9c7e05f8017d9b8c70242
saldanaj27 <jes921@nyu.edu>
2025-12-27 23:30:38 -0500

    update gamebox to different color when game is finished

--- frontend/src/components/GameBox.jsx
+++ frontend/src/components/GameBox.jsx
@@ -1,14 +1,24 @@
 import "./GameBox.css"
 
 export default function GameBox({ game }) {
+  const gameBoxContainer = "gamebox-container"
+  const gameBoxTitle = "gamebox-title"
+  const gameBoxText = "gamebox-text"
+
+  if (game.home_score !== null) {
+    gameBoxContainer += ' finished-game'
+    gameBoxTitle += ' finished-game'
+    gameBoxText += ' finished-game'
+  }
+
   return (
-    <div className="gamebox-container">
-        <h3 className="gamebox-title">
+    <div className={gameBoxContainer}>
+        <h3 className={gameBoxTitle}>
           {game.away_team.abbreviation} @ {game.home_team.abbreviation}
         </h3>
 
-        <p className="gamebox-text">{game.date}</p>
-        <p className="gamebox-text">{game.time}</p>
+        <p className={gameBoxText}>{game.date}</p>
+        <p className={gameBoxText}>{game.time}</p>
     </div>
   )
 }
