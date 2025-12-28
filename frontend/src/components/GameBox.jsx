cb345819d1d8ed3841744e7975d7f80728ace7fb
saldanaj27 <jes921@nyu.edu>
2025-12-28 00:34:33 -0500

    update gamebox component to differentiate between finished and unfinished games

--- frontend/src/components/GameBox.jsx
+++ frontend/src/components/GameBox.jsx
@@ -1,24 +1,47 @@
 import "./GameBox.css"
+import { useNavigate } from 'react-router-dom'
 
 export default function GameBox({ game }) {
-  const gameBoxContainer = "gamebox-container"
-  const gameBoxTitle = "gamebox-title"
-  const gameBoxText = "gamebox-text"
+  const navigate = useNavigate()
+   const isFinished = game.home_score !== null
 
-  if (game.home_score !== null) {
-    gameBoxContainer += ' finished-game'
-    gameBoxTitle += ' finished-game'
-    gameBoxText += ' finished-game'
+  // const gameBoxContainer = "gamebox-container"
+  // const gameBoxTitle = "gamebox-title"
+  // const gameBoxText = "gamebox-text"
+  // const score = "score"
+
+  // if (game.home_score !== null) {
+  //   gameBoxContainer += ' finished-game'
+  //   gameBoxTitle += ' finished-game'
+  //   gameBoxText += ' finished-game'
+  //   score += ' finished-game'
+  // }
+
+  const handleClick = () => {
+    navigate(`/game/${game.id}`)
   }
 
   return (
-    <div className={gameBoxContainer}>
-        <h3 className={gameBoxTitle}>
+    <div
+      className={`gamebox-container ${isFinished ? "finished-game" : ""}`}
+      onClick={handleClick}
+    >
+        <h3 className="gamebox-title">
           {game.away_team.abbreviation} @ {game.home_team.abbreviation}
         </h3>
 
-        <p className={gameBoxText}>{game.date}</p>
-        <p className={gameBoxText}>{game.time}</p>
+        {/* Show score ONLY if finished */}
+        {isFinished && (
+          <p className="score">
+            {game.away_score} – {game.home_score}
+          </p>
+        )}
+
+        <p className="gamebox-text">{game.date}</p>
+        
+        {!isFinished && (
+          <p className="gamebox-text">{game.time}</p>
+        )}
     </div>
   )
 }
