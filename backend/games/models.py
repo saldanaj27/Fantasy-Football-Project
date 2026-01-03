2d924be9ee000fc84defe627eb39e0f529d22c11
saldanaj27 <jes921@nyu.edu>
2026-01-02 21:31:33 -0500

    remove unused fields/classes

--- backend/games/models.py
+++ backend/games/models.py
@@ -50,10 +50,3 @@ class Game(models.Model):
 
     def __str__(self):
         return f"{self.away_team} @ {self.home_team} - Week {self.week}"
-
-# class UserGame(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-
-#     class Meta:
-#         unique_together = ("user", "game")
