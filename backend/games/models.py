9af17f48db55f15c9bc2c94f9a577c3ae30a1a20
saldanaj27 <jes921@nyu.edu>
2026-01-02 21:32:17 -0500

    remove unused fields

--- backend/games/models.py
+++ backend/games/models.py
@@ -2,26 +2,6 @@ from django.db import models
 from teams.models import Team
 from .constants import STAGE_CHOICES
 
-# class Score(models.Model):
-#     quarter_1 = models.IntegerField(default=0)
-#     quarter_2 = models.IntegerField(default=0)
-#     quarter_3 = models.IntegerField(default=0)
-#     quarter_4 = models.IntegerField(default=0)
-#     overtime = models.IntegerField(default=0)
-    
-#     @property
-#     def total(self):
-#         return (
-#             self.quarter_1
-#             + self.quarter_2
-#             + self.quarter_3
-#             + self.quarter_4
-#             + (self.overtime or 0)
-#         )
-
-#     def __str__(self):
-#         return f"Total: {self.total}"
-
 class Game(models.Model): 
     id = models.CharField(max_length=20, primary_key=True)
     season = models.IntegerField(default=0)
@@ -33,10 +13,6 @@ class Game(models.Model):
     away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_team')
     
     stage = models.CharField(max_length=20, choices=STAGE_CHOICES, default='REG')
-
-    # replace if able to retrieve quarter scores
-    # home_score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='home_score')
-    # away_score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='away_score')
 
     home_score = models.IntegerField(default=0, null=True)
     away_score = models.IntegerField(default=0, null=True)
