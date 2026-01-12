ad3a2cb34fb697e628011cec31928c2d57bf1daa
saldanaj27 <jes921@nyu.edu>
2026-01-11 23:33:06 -0500

    add comment that specifies API output

--- frontend/src/api/analytics.js
+++ frontend/src/api/analytics.js
@@ -1,5 +1,20 @@
 import api from "./client";
 
+/*
+  Analytics APIs
+
+  Output of Analytics Object: 
+  {
+    'team_id': <team_id>,
+    'passing': <passing_object>, (attempts, total_yards_average, touchdowns, completion_percentage)
+    'rushing': <rushing_object>, (attempts, total_yards_average, touchdowns)
+    'total_yards_per_game': <float>,
+    'off_turnovers_total': <int>,
+    "def_turnovers_total": <int>,
+    "points_per_game": <float>
+  }
+*/
+
 export async function getRecentStats(numGames, teamId) {
   const response = await api.get(`analytics/recent-stats/?games=${numGames}&team_id=${teamId}`)
   return response.data
