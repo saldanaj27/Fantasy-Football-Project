f7ef5de9381e321236000ddb8bf89bba6a5cb04c
saldanaj27 <jes921@nyu.edu>
2026-01-22 15:55:42 -0500

    update comments

--- frontend/src/api/analytics.js
+++ frontend/src/api/analytics.js
@@ -3,7 +3,9 @@ import api from "./client";
 /*
   Analytics APIs
 
-  Output of Analytics Object: 
+  Output of /api/analytics/recent-stats API:
+  Param: games (default=3), team_id
+
   {
     'team_id': <team_id>,
     'passing': <passing_object>, (attempts, total_yards_average, touchdowns, completion_percentage)
@@ -14,12 +16,26 @@ import api from "./client";
     "points_per_game": <float>
   }
 */
-
 export async function getRecentStats(numGames, teamId) {
   const response = await api.get(`analytics/recent-stats/?games=${numGames}&team_id=${teamId}`)
   return response.data
 }
 
+/*
+  Output of /api/analytics/defense-allowed API:
+  Param: games (default=3), team_id, position (default='RB')
+
+  {
+    'team_id': <team_id>,
+    'position': <string>, (RB, TE, WR, QB)
+    'games_analyzed': <int>,
+    'rushing': <rushing_object>, (attempts, yards, touchdowns)
+    'receiving': <receiving object>, (targets, receptions, yards, touchdowns)
+    'fantasy_points': <float>,
+    'total_yards_allowed': <float>
+  }
+
+*/
 export async function getDefenseAllowed(numGames, teamId, position) {
   const response = await api.get(`analytics/defense-allowed/?games=${numGames}&team_id=${teamId}&position=${position}`)
   return response.data
