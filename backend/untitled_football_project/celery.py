d623fb7b52ae59291ea842f15cb3a799f3ec6201
saldanaj27 <jes921@nyu.edu>
2026-01-26 21:28:54 -0500

    update project name

--- backend/untitled_football_project/celery.py
+++ backend/untitled_football_project/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled_football_project.settings')
 
 app = Celery('football_stats')
 app.config_from_object('django.conf:settings', namespace='CELERY')
